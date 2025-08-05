c8a4749c6051c165bdf125f553d358384c427c17
Shivam Oza <ozashivam03@gmail.com>
2025-08-04 23:14:12 -0400

    Add files via upload

---
+++ Web Traffic Analysis.sql
@@ -0,0 +1,72 @@
+-- Role: eCommerce Database Analyst
+
+-- Web Traffic Analysis
+-- Help CEO Cindy Sharp (CEO) to find where is the traffic coming from
+-- Data Request Date: '2012-04-12'
+SELECT 	utm_source,
+		utm_campaign,
+        http_referer,
+        COUNT(DISTINCT website_session_id) AS sessions
+FROM 	website_sessions
+WHERE 	created_at < '2012-04-12'
+GROUP BY utm_source,
+		 utm_campaign,
+		 http_referer
+ORDER BY sessions DESC;
+
+-- Finding conversion rate for gsearch nonbrand campaigns
+-- Data Request Date: '2012-04-14' 
+SELECT 	utm_source,
+		utm_campaign,
+        COUNT(DISTINCT w.website_session_id) AS sessions,
+        COUNT(DISTINCT o.order_id) AS orders,
+        COUNT(o.order_id) / COUNT(DISTINCT w.website_session_id) AS session_to_order_conv_rt
+FROM 	website_sessions AS w
+	LEFT JOIN orders AS o
+		ON		o.website_session_id = w.website_session_id
+WHERE 	w.created_at < '2012-04-14' 
+	AND		utm_source = 'gsearch'
+	AND		utm_campaign = 'nonbrand'
+GROUP BY utm_source,
+		utm_campaign
+ORDER BY sessions DESC;
+
+-- Tom (Marketing Director) needs gsearch nonbrand trended session volume by week after bidding down gsearch nonbrand
+-- Data Request Date: '2012-05-10' 
+SELECT	WEEK(created_at) AS wk,
+		MIN(DATE(created_at)) AS week_start_date,
+		COUNT(DISTINCT website_session_id) AS sessions
+FROM	website_sessions
+WHERE	created_at < '2012-05-10'
+AND		utm_source = 'gsearch'
+AND		utm_campaign = 'nonbrand'
+GROUP BY 1;
+
+-- Tom requires CVR from session to order by device type for optimize bidding by segments
+-- Data Request Date: '2012-05-11' 
+SELECT 	w.device_type,
+		COUNT(w.website_session_id) AS sessions,
+        COUNT(o.order_id) AS orders,
+        COUNT(o.order_id) / COUNT(w.website_session_id) AS session_to_order_conv_rt 
+FROM 	website_sessions AS w 
+LEFT JOIN orders AS o
+ON		o.website_session_id = w.website_session_id
+WHERE	w.created_at < '2012-05-11'
+AND		utm_source = 'gsearch'
+AND		utm_campaign = 'nonbrand'
+GROUP BY 1;
+
+/*After bidding up the desktop segment, Tom wants to see the results for 
+both desktop and mobile to see the impact of high bids
+between 15th Apirl, 2012 and 6th June, 2012 */
+
+SELECT  -- WEEK(created_at),
+		MIN(DATE(created_at)) AS week_start_date,
+        COUNT(CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS dtop_sessions,
+        COUNT(CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mob_sessions
+		-- COUNT(DISTINCT website_session_id) AS sessions
+FROM	website_sessions
+WHERE	created_at BETWEEN '2012-04-15' AND '2012-06-09'
+AND		utm_source = 'gsearch'
+AND		utm_campaign = 'nonbrand'
+GROUP BY WEEK(created_at);
